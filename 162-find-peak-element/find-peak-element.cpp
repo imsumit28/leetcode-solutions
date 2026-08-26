c10fec520685f8d8bef1d0fe40722a353cf69be7
Sumit Kumar <ersumitkumar45@gmail.com>
2026-08-26 23:15:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (10.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -17,13 +17,11 @@ public:
             else if(nums[mid] > nums[mid-1]){
                 low = mid+1;
             }
-            else if(nums[mid]> nums[mid+1]){
+            else{
                 high = mid-1;
             }
 
-            else{
-                low = mid+1;
-            }
+           
         }
 
         return -1;
