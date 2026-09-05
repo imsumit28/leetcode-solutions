4b36823cbfbc608e1392451be82cd73a4c4221a4
Sumit Kumar <ersumitkumar45@gmail.com>
2026-09-05 18:48:20 +0530

    Time: 7 ms (51.47%) | Memory: 12.3 MB (66.88%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+
+        unordered_map<char,int> value;
+        value['I'] = 1;
+        value['V'] = 5;
+        value['X'] = 10;
+        value['L'] = 50;
+        value['C'] = 100;
+        value['D'] = 500;
+        value['M'] = 1000;
+
+        for(int i = 0;i<s.size();i++){
+            if( i+1<s.size() && value[s[i]]<value[s[i+1]]){
+                    ans -= value[s[i]];
+            } else{
+                ans += value[s[i]];
+            }
+        }
+
+        return ans;
+        
+    }
+};
